8b9c7583793a514d42d932af790cca527f4a3d1a
Alson Foong <alsonfoong2007@gmail.com>
2026-04-21 23:28:35 +0800

    Update twosum.py with explanations

--- easy/twosum.py
+++ easy/twosum.py
@@ -1,3 +1,5 @@
+# Time: O(n) - Single traversal of list, and O(1) on average for hashmap lookup
+# Space: O(n) - Almost every element stored in hashmap in worst-case scenario
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashmap = {}
@@ -7,5 +9,6 @@ class Solution:
                 return [i, hashmap[complement]]
             hashmap[nums[i]] = i
         return []
-# Due to the nature of complements, there is no need to worry about "missing" complements further into the array.
-# When the algorithm eventually passes over the "missed" answer, it will match up with its complement we previously saved to the hashmap.
+# 1. Due to the nature of complements, there is no need to worry about "missing" complements further into the array.
+#    When the algorithm eventually passes over the "missed" answer, it will match up with its complement we previously saved to the hashmap.
+# 2. Checking for the complement before adding the number to the hashmap naturally prevents the algorithm from using the same number twice.
