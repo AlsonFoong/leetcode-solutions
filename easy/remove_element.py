afdbb47ffe7c4e7e6b1c5958f73964fe95c90367
Alson Foong <alsonfoong2007@gmail.com>
2026-04-22 02:37:28 +0800

    Create remove_element.py

---
+++ easy/remove_element.py
@@ -0,0 +1,10 @@
+# Time: O(n) - Single iteration of list
+# Space: O(1) - Single integer variable
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
