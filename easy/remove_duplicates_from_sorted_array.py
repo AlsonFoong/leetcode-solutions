2409f34e850e203c13ff5d6aef74f219f59d7007
Alson Foong <alsonfoong2007@gmail.com>
2026-04-22 17:21:23 +0800

    Create remove_duplicates_from_sorted_array.py

---
+++ easy/remove_duplicates_from_sorted_array.py
@@ -0,0 +1,10 @@
+# Time: O(n) - Single traversal of list
+# Space: O(1) - Single variable declared
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if nums[k] != nums[i]:
+                k += 1
+                nums[k] = nums[i]
+        return k + 1
