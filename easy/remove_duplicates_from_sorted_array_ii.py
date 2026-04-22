555d72461a99a914742294a3a9ae9a546d626b37
Alson Foong <alsonfoong2007@gmail.com>
2026-04-22 22:04:15 +0800

    Create remove_duplicates_from_sorted_array_ii.py

---
+++ easy/remove_duplicates_from_sorted_array_ii.py
@@ -0,0 +1,12 @@
+# Time: O(n) - Single traversal of list
+# Space: O(1) - Two variable declarations
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 2
+        for i in range(2, len(nums)):
+            if nums[k - 2] != nums[i]:
+                nums[k] = nums[i]
+                k += 1
+        return k
+# 1. It helps to see nums[k - 2] as a way to know where to position the k pointer that keeps track of what number needs changing.
+#    When nums[k - 2] == nums[i], it means we've reached a 3rd copy of the same number, and can thus stop incrementing k to keep the pointer right on the number that needs changing.
